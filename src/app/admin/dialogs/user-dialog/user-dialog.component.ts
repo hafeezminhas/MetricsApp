95147c9405350c4310edb521949476a76c57c747
Ammar <ammar4568@gmail.com>
2021-03-16 23:29:48 +0500

    Added Dialog for User

--- src/app/admin/dialogs/user-dialog/user-dialog.component.ts
+++ src/app/admin/dialogs/user-dialog/user-dialog.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { User } from 'src/app/data/models/user';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-user-dialog',
@@ -6,10 +9,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-dialog.component.scss']
 })
 export class UserDialogComponent implements OnInit {
+  user: User;
+  form: FormGroup;
+  update: boolean;
+  emailRegx = /^(([^<>+()\[\]\\.,;:\s@"-#$%&=]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,3}))$/;
+  phoneRegx = /^[(]?\d{3}[)]?[(\s)?.-]\d{3}[\s.-]\d{4}$/g;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder,
+    private dialogRef: MatDialogRef<UserDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) data) {
+    this.user = data.user;
+    this.update = data.update;
   }
 
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.pattern(this.emailRegx)]],
+      phone: ['', [Validators.required, Validators.pattern(this.phoneRegx)]],
+      address: this.fb.group({
+        street: ['', Validators.required],
+        city: ['', Validators.required],
+        state: ['', Validators.required],
+        zip: [null, Validators.required]
+      }),
+      role: ['', Validators.required]
+    });
+  }
+
+  get f(): any {
+    return this.form.controls;
+  }
+
+  onSubmit(): void {
+  }
+
+  close(): void {
+    this.dialogRef.close();
+  }
 }
